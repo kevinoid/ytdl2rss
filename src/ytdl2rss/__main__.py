kevinoid/ytdl2rss

commit d73712ac7c262139f6c89386638661aedd450a26

build: add comment about lack of type support
> There is no provision in PEP 561 for marking a single-file package as
> typed.

- https://github.com/python/typing/discussions/1297
- https://github.com/python/typing/issues/1333

Given the choice between splitting this package into __init__.py and
__main__.py or preserving the single-file ease of use, I'll stick with
ease of use for now.

Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

pyproject.toml
@@ -155,6 +155,11 @@ namespaces = false
 ytdl2rss = [
     # Mark as containing inline type information
     # https://peps.python.org/pep-0561/#packaging-type-information
+    #
+    # FIXME: This doesn't work for single-file modules, since there's no
+    # package directory for py.typed to be copied into.
+    # https://github.com/python/typing/discussions/1297#discussioncomment-4255646
+    # https://github.com/python/typing/issues/1333
     'py.typed',
 ]
 

commit 44ce62ba6ee0fa454b81cb5931f959d04a923c71

Move ytdl2rss.py to src/ytdl2rss/__init__.py
So that the py.typed marker file can be distributed and mark the package
as supporting PEP 561 type information.

Keep the shebang, `__name__ == '__main__'`, and executable permission so
that the file can still be saved and used as a single-file executable
for ease of distribution.  Update README.rst to describe the process.

Provide `__main__.py` for executing the package (rather than the file).

Fixes: d73712ac7c26 ("build: add comment about lack of type support")
Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

pyproject.toml
@@ -155,11 +155,6 @@ namespaces = false
 ytdl2rss = [
     # Mark as containing inline type information
     # https://peps.python.org/pep-0561/#packaging-type-information
-    #
-    # FIXME: This doesn't work for single-file modules, since there's no
-    # package directory for py.typed to be copied into.
-    # https://github.com/python/typing/discussions/1297#discussioncomment-4255646
-    # https://github.com/python/typing/issues/1333
     'py.typed',
 ]
 

src/ytdl2rss/__main__.py
@@ -1,0 +1,8 @@
+#!/usr/bin/env python3
+"""Entry point for running the ytdl2rss package as a command-line tool."""
+
+import sys
+
+from . import main
+
+sys.exit(main())
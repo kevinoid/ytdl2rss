kevinoid/ytdl2rss

commit d18737ffec72830cff38d4a365d5acf718062484

create and use _PROG_NAME constant
The user-facing program name should be consistent, since it's used in
`<generator>` and other user-visible output.  Deducing it from __name__,
__file__, argv[0], or other sources has many edge cases and may change
inadvertently as the code moves.  Define a constant instead.

Moving ytdl2rss.py to ytdl2rss/__init__.py inadvertently broke the tests
by changing <generator> in the output.  This fixes the issue and should
reduce the chance of breaking it in the future.

Fixes: 44ce62ba6ee0 ("Move ytdl2rss.py to src/ytdl2rss/__init__.py")
Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

src/ytdl2rss/__init__.py
@@ -57,6 +57,9 @@ __all__ = [
 ]
 
 _JSON_PATH_KEY = object()
+# User-visible program name.
+# Must be stable and reliable.  Not deduced from __name__, __file__, or argv[0].
+_PROG_NAME = 'ytdl2rss'
 _VERSION_MESSAGE = (
     '%(prog)s '
     + __version__
@@ -654,7 +657,7 @@ def playlist_to_rss(
 
     write(indent2)
     write('<generator>')
-    write(escape(Path(__file__).name + ' ' + __version__))
+    write(escape(_PROG_NAME + ' ' + __version__))
     write('</generator>')
     write(eol)
 
@@ -852,6 +855,7 @@ def _build_argument_parser(
     """
     parser = argparse.ArgumentParser(
         usage='%(prog)s [options] <JSON file...>',
+        prog=_PROG_NAME,
         description=__doc__,
         # Use raw formatter to avoid mangling version text
         formatter_class=argparse.RawDescriptionHelpFormatter,

commit 45c9507f6a6e484f6784a18038050034aa33f268

fix: pass _PROG_NAME to _build_argument_parser
Instead of adding it in _build_argument_parser, which causes

> TypeError: argparse.ArgumentParser() got multiple values for keyword argument 'prog'

Fixes: d18737ffec72 ("create and use _PROG_NAME constant")
Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

src/ytdl2rss/__init__.py
@@ -855,7 +855,6 @@ def _build_argument_parser(
     """
     parser = argparse.ArgumentParser(
         usage='%(prog)s [options] <JSON file...>',
-        prog=_PROG_NAME,
         description=__doc__,
         # Use raw formatter to avoid mangling version text
         formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -915,7 +914,7 @@ def main(argv: Sequence[str] = sys.argv) -> int:
     :return: exit code
     """
     parser = _build_argument_parser(
-        prog=Path(argv[0]).name,
+        prog=_PROG_NAME,
     )
 
     if _HAVE_AUTOCOMPLETE: